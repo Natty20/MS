6499117274d44fb238568778ef9e4486ec6eb1f7
Natty20 <giginatty20@gmail.com>
2024-12-03 17:10:25 +0100

    first commit of gateway

---
+++ api-gateway/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Proxy vers le Auth Service
+app.use('/auth', createProxyMiddleware({
+    target: 'http://localhost:4000/',
+    changeOrigin: true
+}));
+
+// Lancement du serveur
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`API Gateway en cours d'exécution sur le port ${PORT}`));
